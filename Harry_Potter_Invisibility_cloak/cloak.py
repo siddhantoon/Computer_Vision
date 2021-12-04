34b0c4ec6e18c1eed6f5e950c1d5031c78acff95
Siddhant <siddhantoon@gmail.com>
2021-12-04 20:09:36 +0530

    cloak created

---
+++ Harry_Potter_Invisibility_cloak/cloak.py
@@ -0,0 +1,71 @@
+import cv2
+import numpy as np
+
+cloak_color_low = np.array([10, 0, 20])
+cloak_color_high = np.array([50, 255, 255])
+
+
+def roi(image):
+
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
+    # cv2.imshow("img", image)
+
+    mask = cv2.inRange(image, cloak_color_low, cloak_color_high)
+
+    return mask
+
+
+def invisible(img, bg):
+    # img = cv2.imread('')
+    mask = roi(img)
+
+    kernel = np.ones((5, 5), np.uint8)
+    mask = cv2.erode(mask, kernel, iterations=1)
+
+    negative_mask = cv2.bitwise_not(mask)
+    # cv2.imshosw("neg", negative_mask)
+    # cv2.imshow("mask", mask)
+    cloak = cv2.bitwise_and(bg, bg, mask=mask)
+    non_cloak = cv2.bitwise_and(img, img, mask=negative_mask)
+    output = non_cloak + cloak
+    return output
+
+
+if __name__ == '__main__':
+
+    # image = cv2.imread("imgs/opencv_frame_4.jpg")
+    # img = np.copy(image)
+    bg = cv2.imread("background.jpg")
+    background = np.copy(bg)
+
+    cap = cv2.VideoCapture(0)
+    print("Press Escape to Quit (x)")
+    face_cascade = cv2.CascadeClassifier('face.xml')
+
+    while True:
+        ret, frame = cap.read()
+        frame2 = np.copy(frame)
+        harry = np.copy(frame)
+
+        faces = face_cascade.detectMultiScale(frame2)
+
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame2, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        if type(faces) is tuple:
+            # When No faces are deteccted
+            harry = invisible(frame, background)
+
+        cv2.imshow("Cloak", harry)
+
+        cv2.imshow("face", frame2)
+        k = cv2.waitKey(1)
+
+        if k % 256 == 27:
+            print("Escape Hit, closing...")
+            break
+
+    # harry = invisible(img, background)
+    # cv2.imshow("original ", image)
+    # cv2.imshow("invisible", harry)
+    # cv2.waitKey(0)
